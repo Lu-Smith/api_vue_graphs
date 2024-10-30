1c582d21007551bf93019a8079f085997ba8585a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-30 11:21:19 +0000

    Added api data to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import {getProductBenchmarks, getExchangeRates} from './data/api';
+
+onMounted(async () =>{
+  try {
+    console.log('product', getExchangeRates());
+    console.log('rates', getProductBenchmarks());
+  }
+  catch (error) {
+    console.error('Failed to fetch data:', error)
+  }
+})
 </script>
 
 <template>
   <h1>We Are Graphs</h1>
+
 </template>
 
 <style scoped>

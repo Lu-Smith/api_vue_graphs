8ccef932525b20678f60eff72025c28b74c93698
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 15:07:48 +0000

    Updated GraphComponent.vue

--- src/components/GraphComponent.vue
+++ src/components/GraphComponent.vue
@@ -1,21 +1,77 @@
 <template>
   <h2>Graph</h2>
-  <div ref="chart" style="width: 100%; height: 400px;"></div>
+  <div :id="chartId" style="width: 100%; height: 400px;"></div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { onMounted, onBeforeUnmount } from 'vue';
 import * as echarts from 'echarts';
 import { Transaction } from '../assets/intefaces';
 
-defineProps<{ transactions: Transaction[] }>();
+const props = defineProps<{ transactions: Transaction[], chartId: string }>();
 
-const chartRef = ref<HTMLElement | null>(null);
+let myChart: echarts.ECharts | null = null;
 
 onMounted(() => {
-  if (chartRef.value) {
-    const myChart = echarts.init(chartRef.value);
-  }
+  myChart = echarts.init(document.getElementById(props.chartId) as HTMLElement);
+
+  const labels = props.transactions.map(tx => tx.start_date); 
+  const paymentData = props.transactions.map(tx => tx.payment);
+  const benchmarkData = props.transactions.map(tx => tx.benchmark);
+
+  //chart
+
+  const options = {
+    title: {
+      text: 'Payment vs Benchmark',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      data: ['Payment (€)', 'Benchmark (€)'],
+    },
+    xAxis: {
+      type: 'category',
+      data: labels,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Payment (€)',
+        type: 'bar',
+        data: paymentData,
+        emphasis: {
+          focus: 'series',
+        },
+      },
+      {
+        name: 'Benchmark (€)',
+        type: 'bar',
+        data: benchmarkData,
+        emphasis: {
+          focus: 'series',
+        },
+      },
+    ],
+  };
+
+  myChart.setOption(options);
+
+
+  //Resize
+  window.addEventListener('resize', () => {
+    myChart?.resize();
+  });
+
+  // Cleanup
+  onBeforeUnmount(() => {
+    window.removeEventListener('resize', () => myChart?.resize());
+    myChart?.dispose();
+  });
+
 });
 
 //chart 

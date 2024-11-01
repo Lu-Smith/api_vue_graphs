ec9b9f4140987d7245de9e5450e9bc49918b31d1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 14:48:11 +0000

    Updated GraphComponent.vue

--- src/components/GraphComponent.vue
+++ src/components/GraphComponent.vue
@@ -1,9 +1,22 @@
 <template>
   <h2>Graph</h2>
+  <div ref="chart" style="width: 100%; height: 400px;"></div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts';
 import { Transaction } from '../assets/intefaces';
 
 defineProps<{ transactions: Transaction[] }>();
+
+const chartRef = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  if (chartRef.value) {
+    const myChart = echarts.init(chartRef.value);
+  }
+});
+
+//chart 
 </script>

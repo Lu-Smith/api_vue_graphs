d0b7fc11c3526af3f06b7054bb9db038d2828794
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 13:34:24 +0000

    Updated sorting in TableComponent.vue

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -6,18 +6,28 @@
         <th> Payment (€)</th>
         <th> Benchmark (€)</th>
         <th> Difference (€)</th>
-        <th> Start Date (€)</th>
-        <th> End Date (€)</th>
+        <th @click="changeSort('start_date')"> 
+          Start Date (€) 
+          <span v-if="currentSort === 'start_date'">
+            {{ sortDirection === 'asc' ? '▲' : '▼' }}
+          </span>
+        </th>
+        <th @click="changeSort('end_date')"> 
+          End Date (€)
+          <span v-if="currentSort === 'end_date'">
+            {{ sortDirection === 'asc' ? '▲' : '▼' }}
+          </span>
+        </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(transaction, index) in props.transactions" :key="index">
+      <tr v-for="(transaction, index) in sortTransactions" :key="index">
         <td>{{ transaction[oppositeKey] }}</td>
         <td>{{ transaction.payment.toFixed(2) }}</td>
         <td>{{ transaction.benchmark.toFixed(2) }}</td>
         <td>{{ (transaction.payment - transaction.benchmark).toFixed(2) }}</td>
         <td>{{ transaction.start_date }}</td>
-        <td>{{ transaction.end_date }}</td>
+        <td >{{ transaction.end_date }}</td>
       </tr>
       <tr>
         <td>Total Difference (€): </td>
@@ -28,7 +38,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { Transaction } from '../assets/intefaces';
 
 const props = defineProps<{ transactions: Transaction[], keyName: 'provider_name' | 'product_name'}>();
@@ -46,4 +56,28 @@ const totalDifference  = computed(() => {
   }, 0);
 });
 
+//sort
+const currentSort = ref<'start_date' | 'end_date'>('start_date'); 
+const sortDirection = ref<'asc' | 'desc'>('asc');
+
+const changeSort = (column: ('start_date' | 'end_date')) => {
+  if(column === 'start_date') {
+    currentSort.value = 'start_date';
+  } else {
+    currentSort.value = 'end_date';
+  }
+  sortDirection.value = sortDirection.value === 'asc' ? 'desc' : 'asc';
+};
+
+const sortTransactions = computed(() => {
+  const sorted = [...props.transactions];
+
+  sorted.sort((a, b) => {
+    const comparision = new Date(a[currentSort.value]).getTime() - new Date(b[currentSort.value]).getTime();
+    
+    return sortDirection.value === 'asc' ? comparision : -comparision;
+  })
+
+  return sorted
+})
 </script>

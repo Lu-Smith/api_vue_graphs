d22759c8a84031f0ebc32cc3f3778381008505c3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 11:32:19 +0000

    Updated TableComponent.vue

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,3 +1,29 @@
 <template>
-  Table
+  <table>
+    <thead>
+      <tr>
+        <th> Payment (€)</th>
+        <th> {{ name }} (€)</th>
+        <th> Difference (€)</th>
+        <th> Start Date (€)</th>
+        <th> End Date (€)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { Transaction } from '../assets/intefaces';
+
+const props = defineProps<{ transactions: Transaction[], keyName: string }>();
+
+const name = computed(() => (props.keyName !== 'providerName' ? 'Provider' : 'Product'));
+</script>

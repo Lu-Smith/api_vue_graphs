32b30c87f6cb5f3cc3afedab0e29d6f4ca889344
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 10:22:43 +0000

    Updated interface.ts

--- src/assets/intefaces.ts
+++ src/assets/intefaces.ts
@@ -1,3 +1,25 @@
+export interface ProductBenchmarks {
+  id : number,
+  product_name: string,
+  provider_name: string;
+  end_date : string,
+  start_date : string,
+  currency : {
+    id: number,
+    name: string,
+    symbol: string,
+  },
+  payment : number,
+  benchmark : number
+}
+
+export interface ExchangeRates {
+  exchange_rate : number,
+  from_currency_id : number,
+  to_currency_id : number,
+  year : number
+}
+
 export interface Transaction {
   provider_name: string;
   product_name: string,

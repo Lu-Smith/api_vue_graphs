2d107dcb6187dc9c19a442c5d4c6ee58ad4c3b19
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 10:23:07 +0000

    Created PaymentBenchmarkComponent.vue

---
+++ src/components/PaymentBenchmarksComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Hello I am Table</h2>
+</template>
+
+<script setup lang="ts">
+import { Transaction } from '../assets/intefaces';
+
+defineProps<{ transactions: Transaction[] }>();
+</script>

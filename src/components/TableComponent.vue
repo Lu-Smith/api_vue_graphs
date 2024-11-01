fb2f09cc7846d6f396fe52eec8a32c8c5eeb6f9c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-01 11:50:56 +0000

    Updated TableComponent.vue

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -2,18 +2,22 @@
   <table>
     <thead>
       <tr>
-        <th> Payment (€)</th>
         <th> {{ name }} (€)</th>
+        <th> Payment (€)</th>
+        <th> Benchmark (€)</th>
         <th> Difference (€)</th>
         <th> Start Date (€)</th>
         <th> End Date (€)</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>
-
-        </td>
+      <tr v-for="(transaction, index) in props.transactions" :key="index">
+        <td>{{ transaction[oppositeKey] }}</td>
+        <td>{{ transaction.payment }}</td>
+        <td>{{ transaction.benchmark }}</td>
+        <td>{{ transaction.payment - transaction.benchmark }}</td>
+        <td>{{ transaction.start_date }}</td>
+        <td>{{ transaction.end_date }}</td>
       </tr>
     </tbody>
   </table>
@@ -23,7 +27,9 @@
 import { computed } from 'vue';
 import { Transaction } from '../assets/intefaces';
 
-const props = defineProps<{ transactions: Transaction[], keyName: string }>();
+const props = defineProps<{ transactions: Transaction[], keyName: 'provider_name' | 'product_name'}>();
 
-const name = computed(() => (props.keyName !== 'providerName' ? 'Provider' : 'Product'));
+const name = computed(() => (props.keyName !== 'provider_name' ? 'Provider' : 'Product'));
+
+const oppositeKey = computed(() => (props.keyName === 'provider_name' ? 'product_name' : 'provider_name'));
 </script>
